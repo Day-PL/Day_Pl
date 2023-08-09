fe770860524fbcbbab1b15c2e991382d03dd6b84
JaeY0ungJung <hanol5216@gmail.com>
2023-08-09 20:43:51 +0900

    refactor : Course -> Plan 으로 변경, db 테이블명 변경에 따른 변경

--- Day_Pl/admin.py
+++ Day_Pl/admin.py
@@ -1,9 +1,7 @@
 from django.contrib import admin
 from .models import *
 
-#? Register your models here.
-admin.site.register(UserCourse)
-admin.site.register(UserCourseLike)
+admin.site.register(PlaceType)
 admin.site.register(Place)
-admin.site.register(UserPlaceLike)
+admin.site.register(Plan)
 admin.site.register(UserPreference)
