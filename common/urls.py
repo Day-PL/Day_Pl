262f9e9b6986f52fe7ea44876ded03e944a19ade
JaeY0ungJung <hanol5216@gmail.com>
2023-08-09 22:34:34 +0900

    init :  urls api 명세서 수정하면서 함께 변경

--- common/urls.py
+++ common/urls.py
@@ -7,5 +7,7 @@ app_name = 'common'
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='auth/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('signup/', views.signup, name='signup'),
+    path("", views.index, name='index'),
+    path('signup/', views.signup, name='signup'), #! 회원가입
+    path('info/', views.info, name='info'), #! 회원정보 수정, 회원 탈퇴
 ]
