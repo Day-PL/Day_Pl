344d1b4ad679549c958fc4bf9988dd92c41506fa
JaeY0ungJung <hanol5216@gmail.com>
2023-08-09 20:50:16 +0900

    init : flask에서 django로 바꾸면서 views 내 모든 파일 초기화

--- Day_Pl/views/browse.py
+++ Day_Pl/views/browse.py
@@ -1,12 +1,3 @@
-from flask import Blueprint, request, render_template
-from flask_login import current_user
-
-bp = Blueprint('browse', __name__, url_prefix='/browse')
-
 # <메인 페이지>
 # 인기 TOP3 코스 보여주기
 # 나의 플랜 만들기
-
-@bp.route("/")
-def browse():
-    return render_template("browse.html", current_user = current_user)
