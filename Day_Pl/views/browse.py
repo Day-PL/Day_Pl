90dfa65ee1c49b5969c7983f95b09a119ff34b43
JaeY0ungJung <hanol5216@gmail.com>
2023-08-10 20:43:13 +0900

    init : 모든 페이지 연결

--- Day_Pl/views/browse.py
+++ Day_Pl/views/browse.py
@@ -1,3 +1,8 @@
-# <메인 페이지>
-# 인기 TOP3 코스 보여주기
-# 나의 플랜 만들기
+from django.shortcuts import render, redirect
+# from ..models import 
+
+#! <메인 페이지>
+#! 인기 TOP3 코스 보여주기
+#! 나의 플랜 만들기
+def index(request):
+    return render(request, 'browse.html')
