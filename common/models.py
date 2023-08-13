dbe4e4ee9a4ce791db233211dc39086e516184ea
Minjoo <m0522inju@gmail.com>
2023-08-13 23:21:25 +0900

    feat: 회원가입 기능 구현(약관 및 닉네임 제외)
    - layout.html :  javascript와 연결 위하여 script 태그 추가
    - urls.py : API 명세서에 따라 기존 'signup/'으로 기재되어 있던 url을 'new-user/'로 변경
    - models.py : uuid : UUIDField 사용 / image 컬럼의 default값 추가
    - 약관 : 추후 구체적인 약관 정해진 이후 추가 예정
    - 닉네임 : main에서 마이그레이션 완료 후 추가 예정

--- common/models.py
+++ common/models.py
@@ -1,16 +1,21 @@
+import uuid
 from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    uuid       = models.CharField(max_length=64, blank=False)
+    user       = models.OneToOneField(User, on_delete=models.CASCADE)
+    uuid       = models.UUIDField(unique=True, blank=False, default=uuid.uuid4)
     fullname   = models.CharField(max_length=64, blank=False)
+    # nickname   = models.CharField(max_length=64, blank=True)
     gender     = models.CharField(max_length=64, blank=False)  
     birthdate  = models.DateField(max_length=64, blank=False)
     phone      = models.CharField(max_length=64, blank=False)
     mail       = models.CharField(max_length=64, blank=False)
     rq_terms   = models.BooleanField(default=0)
     op_terms   = models.BooleanField(default=0)
-    image      = models.ImageField(upload_to='photos/', null=True)
-    sign_date  = models.DateTimeField(auto_now_add=True)  
+    image      = models.ImageField(upload_to='photos/', null=True, default='static/img/profile_img.png')
+    sign_date  = models.DateTimeField(auto_now_add=True) 
+    
+    def __str__(self):
+        return self.user.username
--- common/urls.py
+++ common/urls.py
@@ -8,6 +8,8 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='auth/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     # path("", views.index, name='index'),
-    # path('signup/', views.signup, name='signup'), #! 회원가입
+    path('new-user/', views.signup, name='signup'), #! 회원가입
+    path('new-user/check-username/', views.check_username, name='check-username'),
+    path('new-user/check-mail/', views.check_mail, name='check-mail'),
     # path('info/', views.info, name='info'), #! 회원정보 수정, 회원 탈퇴
 ]
