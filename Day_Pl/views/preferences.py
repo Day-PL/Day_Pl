ed5d4602542e6d82013b463388033d796791fdb9
JaeY0ungJung <hanol5216@gmail.com>
2023-08-14 20:07:43 +0900

    feat : 선호도 조사 창 db 연결까지 구현

--- Day_Pl/views/preferences.py
+++ Day_Pl/views/preferences.py
@@ -4,63 +4,37 @@ from ..models import Preference, PlaceType
 
 @login_required
 def index(request):
-    preference_choices = [placetype.code for placetype in PlaceType.objects.all()]
-    # print(preference_choices)
+    print(f"현재 로그인 유저: {request.user}  id(pk): {request.user.id}")
 
-    user_preference = Preference.objects.get(user = request.user).prefer.code
+    preference_choices = PlaceType.objects.all() #! preference_choices의 각 원소는 html 요소의 id값으로 쓰임
+    # for x in preference_choices:
+        # print(f"[디버깅] {x.code} {x.name}")
 
+    user_preference_ids = [user_preference.prefer.pk for user_preference in  Preference.objects.filter(user = request.user)]
+    print(f"[디버깅] user_preference_ids: {user_preference_ids}")
 
-    context = {'preference_choices' : preference_choices,
-               }
+    if request.method == 'POST':
+        for preference_choice in preference_choices:
+            print(f"[디버깅] {preference_choice.code} : {request.POST.get(preference_choice.code, 'off')}")
+            if request.POST.get(preference_choice.code, 'off') == 'on' and preference_choice.pk not in user_preference_ids:
+                #! db에 추가
+                Preference.objects.create(prefer_id = preference_choice.pk, user_id = request.user.id)
+            if request.POST.get(preference_choice.code, 'off') == 'off' and preference_choice.pk in user_preference_ids:
+                #! db에서 제거
+                Preference.objects.get(prefer_id = preference_choice.pk, user_id = request.user.id).delete() 
+        return redirect('Day_Pl:preferences')
+        
+    context = {
+        'preference_choices' : preference_choices,
+        'user_preference_ids'   : user_preference_ids,
+        'code_alphabets' : {'A': '먹거리',
+                            'B': '일상 데이트', 'C':'이색카페', 'D':'실내스포츠', 'E':'실외스포츠', 'F':'볼거리', 'G':'특별 데아트'}
+        }
     return render(request, 'preferences.html' , context = context)
 
 
 
-# @login_required
-# def index(request):
-#     #! 기존의 preference 불러오기 (회원가입 완료즉시 False로 만들어줘야함 -> 이후 이 창 뜸)
-#     #? 다른 방법은 없을까?
-#     user_preference = Preference.objects.get(id = request.user.id)
-#     # user_preference = UserPreference.objects.get(user = request.user)
-#     # print(2, user_preference)
-    
-#     #! 유저의 UserPreference 테이블 데이터 가져오기 (True / False)
-#     content = {
-#         'amusement_park' : user_preference.amusement_park,
-#         'zoo' : user_preference.zoo,
-#         'plant' : user_preference.plant,
-#         'walk' : user_preference.walk,
-#         'bike' : user_preference.bike,
-#         'skate' : user_preference.skate,
-#         'hike' : user_preference.hike,
-#         'aquarium' : user_preference.aquarium,
-#         'spa' : user_preference.spa,
-#         'shopping' : user_preference.shopping,
-#         'movie' : user_preference.movie,
-#         'cafe' : user_preference.cafe,
-#         'cartoon' : user_preference.cartoon,
-#         'dog' : user_preference.dog,
-#         'cat' : user_preference.cat,
-#         'cooking' : user_preference.cooking,
-#         'perfume' : user_preference.perfume,
-#         'ceramic' : user_preference.ceramic,
-#         'ring' : user_preference.ring,
-#         'alcohol': user_preference.alcohol,
-#         'drawing' : user_preference.drawing,
-#         'dance' : user_preference.dance,
-#         'flower' : user_preference.flower,
-#         'boardgame' : user_preference.boardgame,
-#         'escaperoom' : user_preference.escaperoom,
-#         'gameroom' : user_preference.gameroom,
-#         'screen_baseball' : user_preference.screen_baseball,
-#         'bawling' : user_preference.bawling,
-#         'musical' : user_preference.musical,
-#         'play' : user_preference.play,
-#         'concert' : user_preference.concert,
-#         'exhibition' : user_preference.exhibition
-#     }
-#     print(f'before: {content}')
-
+#
 #     #! 데이터 받아서
 #     if request.method == 'POST':
 #         for key in content.keys():
