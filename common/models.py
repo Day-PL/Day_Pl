aedc6b9e9f88d0326394ac7982fb6109bc4ad6b6
JaeY0ungJung <hanol5216@gmail.com>
2023-08-14 15:55:51 +0900

    add : db model 변경

--- common/models.py
+++ common/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
+import uuid
 
 # Create your models here.
 class Profile(models.Model):
     user       = models.OneToOneField(User, on_delete=models.CASCADE)
-    uuid       = models.CharField(max_length=64, blank=False)
-    # nickname   = models.CharField(max_length=64, blank=False) #! 추가 예정
+    uuid       = models.UUIDField(unique=True, max_length=64, default=uuid.uuid4)
+    nickname   = models.CharField(max_length=64, blank=False) #! 추가 예정
     fullname   = models.CharField(max_length=64, blank=False)
     gender     = models.CharField(max_length=64, blank=False)  
     birthdate  = models.DateField(max_length=64, blank=False)
@@ -13,5 +14,5 @@ class Profile(models.Model):
     mail       = models.CharField(max_length=64, blank=False)
     rq_terms   = models.BooleanField(default=0)
     op_terms   = models.BooleanField(default=0)
-    image      = models.ImageField(upload_to='photos/', null=True)
+    image      = models.ImageField(upload_to='photos/', null=True, default='static/img/profile_img.png')
     sign_date  = models.DateTimeField(auto_now_add=True)  
