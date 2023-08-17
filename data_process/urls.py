5afe1a627582e7cc47f776067d023315da9d7e89
Minjoo <m0522inju@gmail.com>
2023-08-17 12:21:00 +0900

    feat : 전시회 크롤링한 데이터 db로 업데이트하는 코드 및 화면 구현

--- data_process/urls.py
+++ data_process/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import naver_place_crawler, naver_place_csv_to_db_all
+from .views import naver_place_crawler, naver_place_csv_to_db_all, exhibit_save_to_db
 
 app_name = "data_process"
 
 urlpatterns = [
     path("naver_place_crawler/",       naver_place_crawler,       name = 'naver_place_crawler'), 
     path("naver_place_csv_to_db_all/", naver_place_csv_to_db_all, name = 'naver_place_csv_to_db_all'),
-
+    path("exhibit_save_to_db/<str:date>/", exhibit_save_to_db, name = 'exhibit_save_to_db')
 ]
