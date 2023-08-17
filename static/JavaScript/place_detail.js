cfb7a8a241659bd93e248cf0dcd60ea982bc1bb1
JaeY0ungJung <hanol5216@gmail.com>
2023-08-17 16:34:38 +0900

    feat : 지도창과 장소디테일창 div 동시에 보이지 않게 변경

--- static/JavaScript/place_detail.js
+++ static/JavaScript/place_detail.js
@@ -1,11 +1,20 @@
 const placeNames = document.querySelectorAll('.placeName');
+
 placeNames.forEach(placeName => {
     placeName.addEventListener('click', function(){
+        //! 모두 안 보이게 하기
+        placeNames.forEach(place => {
+            var placeId = place.id
+            document.getElementById('detail_' + placeId).style.display='none';
+            console.log(place, '닫기');
+        });
+        //! 클릭한 것만 보이게 하기
         var place_id = placeName.id
-        console.log('detail_' + place_id)
         document.getElementById('div3').style.display='none';
-        // document.getElementById('div4').style.display='block'; 
-        document.getElementById('detail_' + place_id).style.display='block'
+        document.getElementById('div4').style.display='block';
+        document.getElementById('detail_' + place_id).style.display='block';
+        console.log(place_id, '열기')
+
     });
 });
 
@@ -14,5 +23,6 @@ placeDetails.forEach(placeDetail => {
     placeDetail.addEventListener('click', function(){
         placeDetail.parentElement.style.display='none';
         document.getElementById('div3').style.display='block';
+        document.getElementById('div4').style.display='none';
     });
 });
