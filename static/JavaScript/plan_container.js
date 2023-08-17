eecb41f76b0df01912db93a28f713ca07e452c6f
Minjoo <m0522inju@gmail.com>
2023-08-17 18:25:16 +0900

    feat : 플랜 만들기 - 플랜 저장 기능 추가

--- static/JavaScript/plan_container.js
+++ static/JavaScript/plan_container.js
@@ -1,18 +1,20 @@
-const plusBtn = document.querySelector('.place-plus__btn')
+const planSubmitBtn = document.querySelector('.plan-submit__btn')
 const selectedPlaceContainer = document.querySelector('.selected-place__container')
-
 const placeContainer = document.querySelector('.place_container')
 
 // TODO: 하드코딩 변수 옮기기
 PLACE_CURRENT = 1;
 PLACE_TOTAL = 10;
 
-function addPlaceItem(placeName) {
+function addPlaceItem(placeName, placeId) {
   let uuid = self.crypto.randomUUID();
 
+  // placeid를 이용하지 않고 uuid를 사용하는 이유 :
+  // uuid 기반으로 삭제되기 때문에 같은 장소 두 번 추가해도 선택한 값만 삭제되도록 하기 위해
   const placeBox = document.createElement('li');
   placeBox.setAttribute('class', 'place__box');
   placeBox.setAttribute('data-id', uuid)
+  placeBox.setAttribute('data-place', `${placeId}`)
   placeBox.innerHTML = `
     <div class="place__item">
       <span class="place-like__btn">
@@ -38,12 +40,48 @@ selectedPlaceContainer.addEventListener('click', event => {
 
 placeContainer.addEventListener('click', event => {
   const placeId = event.target.dataset.place;
-  console.log(placeId)
   if (placeId && PLACE_CURRENT < PLACE_TOTAL) {
     const placeName = document.getElementById(`${placeId}`).innerText;
-    console.log(placeName)
-    const placeBox = addPlaceItem(placeName)
+    const placeBox = addPlaceItem(placeName, placeId)
     selectedPlaceContainer.appendChild(placeBox)
     PLACE_CURRENT++;
   }
 })
+
+planSubmitBtn.addEventListener('click', () => {
+  const planTitle = getPlanTitle()
+  // TODO: 등록된 장소들 찾기
+  const placeBoxItems = selectedPlaceContainer.querySelectorAll('.place__box');
+  const placeIds = Array.from(placeBoxItems).map(item => item.dataset.place);
+  console.log(placeIds)
+
+  fetch('', {
+    method: 'POST',
+    headers: {
+      'X-CSRFToken': csrfToken,
+      'Content-Type': 'application/json',
+    },
+    body : JSON.stringify({ 
+      plantitle: planTitle,
+      placeids: placeIds }),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.status === 'success') {
+        location.reload()
+      } else {
+        console.log('에러 발생')
+      }
+    })
+})
+
+function getPlanTitle() {
+  const planTitleInput = document.querySelector('.plan-title');
+  let planTitle = ''
+  if (planTitleInput.value === '') {
+    planTitle = planTitleInput.getAttribute('placeholder');
+  } else {
+    planTitle = planTitleInput.value;
+  }
+  return planTitle
+}
