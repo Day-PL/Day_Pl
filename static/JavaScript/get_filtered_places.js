bdfa48af9abd3a35ce280dc797e4b3a58aae6fc7
JaeY0ungJung <hanol5216@gmail.com>
2023-08-18 14:55:55 +0900

    fix : javascript를 통해 장소 필터링하는 곳 UI 통일

--- static/JavaScript/get_filtered_places.js
+++ static/JavaScript/get_filtered_places.js
@@ -21,7 +21,7 @@ function getFilteredPlace(){
 
 function showPlace(place){
     const placeInfo = place['fields'];
-    
+    const placeId = place['id'];
     const name = placeInfo['name'];
     const rating = placeInfo['rating'];
     const reviewTotal = placeInfo['review_total'];
@@ -33,11 +33,34 @@ function showPlace(place){
     div.setAttribute('class', 'card')
     div.innerHTML = 
     `
-        <div class="card-body">
-        <a href="https://map.naver.com/v5/search/${addressGu} ${addressLo} ${name}/place" target="_blank"><h6 class="card-title">${name}</h6></a>
-        <p class="card-text">별점: ${rating}  리뷰수: ${ reviewTotal }</p>
-        <p class="card-text">${addressGu} ${addressLo} ${addressDetail}</p>
-        <a href="https://map.naver.com/v5/directions/-/14129228.684381623,4517601.068035996,${addressGu} ${addressLo} ${name},1151030658,PLACE_POI/-/transit?c=15,0,0,0,dh&isCorrectAnswer=true" target="_blank">길찾기</a>
+        <div id="place_${placeId}" class="card" style="height:90px; width: 18rem;">
+            <div class="card-body">
+                <h6 class="card-title">
+                    <span class="placeName" id="${placeId}">
+                        <font size=2>${name}</font>
+                    </span>
+                    &nbsp;
+                    <a href="https://map.naver.com/v5/search/${addressGu} ${addressLo} ${name}/place" target="_blank">
+                        <font size=1>네이버플레이스</font>
+                    </a>
+                    &nbsp;
+                    <a href="https://map.naver.com/v5/directions/-/14129228.684381623,4517601.068035996,${addressGu} ${addressLo} ${name},1151030658,PLACE_POI/-/transit?c=15,0,0,0,dh&isCorrectAnswer=true" target="_blank">
+                        <font size=1>길찾기</font>
+                    </a>
+                </h6>   
+                <font size=1>
+                    <p class="card-text">
+                        별점: ${rating}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;리뷰수: ${ reviewTotal }
+                    </p>
+                </font>
+                <font size=1>
+                    <p class="card-text">
+                        ${addressGu} ${addressLo} ${addressDetail}
+                    </p>
+                </font>
+                <button data-place="${placeId}">플랜에 추가</button>
+            </div>
+        </div>
     `
     return div
 }
