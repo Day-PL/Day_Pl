32453289a414e92ebb2b9d010476e81b2e7446f8
JaeY0ungJung <hanol5216@gmail.com>
2023-08-24 18:01:58 +0900

    add : plan 좋아하는 사람 수 출력 코드

--- Day_Pl/views/populars.py
+++ Day_Pl/views/populars.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render, redirect
-# from ..models import 
+from ..models import Place, PlaceType, Plan, PlanPlace, Preference
 
 def index(request):
+    plans = Plan.objects.all()
+    for plan in plans:
+        print(plan.title)
+        print(plan.like_users.count())
     return render(request, 'populars.html')
 
 def detail(request):
