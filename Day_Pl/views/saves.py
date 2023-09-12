311fb77d79a2c047dae07481ff4a0e3482f8d5ce
JaeY0ungJung <hanol5216@gmail.com>
2023-09-12 16:17:57 +0900

    feat : saves 디자인 통일 및 기능 구현 - 이벤트 버블링 방지 (하트 클릭, plan box 클릭 시)

--- Day_Pl/views/saves.py
+++ Day_Pl/views/saves.py
@@ -7,10 +7,10 @@ from datetime import date
 
 
 def index(request):
-    user = request.user
-    like_plans = user.like_plans.all()
-    view_plans = user.view_plans.all()
-    my_plans   = Plan.objects.filter(user=user)
+    login_user = request.user
+    like_plans = login_user.like_plans.all()
+    view_plans = login_user.view_plans.all()
+    my_plans   = Plan.objects.filter(user=login_user)
 
     like_plans_processed, view_plans_processed, my_plans_processed = [], [], []
     plans_places, check_planid = [], []
@@ -18,11 +18,19 @@ def index(request):
     #! 내가 좋아하는 플랜들
     for plan in like_plans:
         plan_dict = {
-            'id' : plan.id,
-            'uuid' : plan.uuid,
-            'title' : plan.title,
+            'create_user': plan.user,
+            'id'         : plan.id,
+            'uuid'       : plan.uuid,
+            'title'      : plan.title,
+            'created_at' : plan.created_at,
+            'modified_at'  : plan.modified_at,
+            'hashtag_area' : plan.hashtag_area,
+            'hashtag_type' : plan.hashtag_type,
+            'hashtag_pick' : plan.hashtag_pick,
+            'memo'       : plan.memo,
+            'total_time' : plan.total_time,
+            'public'     : plan.public,
             'like_count' : plan.like_users.count(),
-            'public' : plan.public,
         }
         plan_places = PlanPlace.objects.filter(plan = plan).order_by('order')
         #! 중복처리
@@ -34,11 +42,19 @@ def index(request):
     #! 내가 본 플랜들
     for plan in view_plans:
         plan_dict = {
-            'id' : plan.id,
-            'uuid' : plan.uuid,
-            'title' : plan.title,
+            'create_user': plan.user,
+            'id'         : plan.id,
+            'uuid'       : plan.uuid,
+            'title'      : plan.title,
+            'created_at' : plan.created_at,
+            'modified_at'  : plan.modified_at,
+            'hashtag_area' : plan.hashtag_area,
+            'hashtag_type' : plan.hashtag_type,
+            'hashtag_pick' : plan.hashtag_pick,
+            'memo'       : plan.memo,
+            'total_time' : plan.total_time,
+            'public'     : plan.public,
             'like_count' : plan.like_users.count(),
-            'public' : plan.public,
         }
         plan_places = PlanPlace.objects.filter(plan = plan).order_by('order')
         #! 중복처리
@@ -46,8 +62,7 @@ def index(request):
             plans_places.append(plan_places)
             check_planid.append(plan.id)
         #! 내가 좋아하는 것인지 판별
-        # if plan.like_users.filter(id=user.id).exists():
-        if user.like_plans.filter(id=plan.id).exists():
+        if login_user.like_plans.filter(id=plan.id).exists():
             plan_dict['islike'] = True
         else:
             plan_dict['islike'] = False
@@ -56,18 +71,26 @@ def index(request):
     
     for plan in my_plans:
         plan_dict = {
-            'id' : plan.id,
-            'uuid' : plan.uuid,
-            'title' : plan.title,
+            'create_user': plan.user,
+            'id'         : plan.id,
+            'uuid'       : plan.uuid,
+            'title'      : plan.title,
+            'created_at' : plan.created_at,
+            'modified_at'  : plan.modified_at,
+            'hashtag_area' : plan.hashtag_area,
+            'hashtag_type' : plan.hashtag_type,
+            'hashtag_pick' : plan.hashtag_pick,
+            'memo'       : plan.memo,
+            'total_time' : plan.total_time,
+            'public'     : plan.public,
             'like_count' : plan.like_users.count(),
-            'public' : plan.public,
         }
         plan_places = PlanPlace.objects.filter(plan = plan).order_by('order')
         #! 중복처리
         if plan.id not in check_planid:
             plans_places.append(plan_places)
             check_planid.append(plan.id)
-        if user.like_plans.filter(id=plan.id).exists():
+        if login_user.like_plans.filter(id=plan.id).exists():
             plan_dict['islike'] = True
         else:
             plan_dict['islike'] = False
@@ -81,36 +104,36 @@ def index(request):
             PlanPlace.objects.filter(id = plan_places[i].id).update(road_url=road_url)
 
     context = {
-        'plans_places': plans_places,
+        'plans_places'        : plans_places,
         'like_plans_processed': like_plans_processed,
         'view_plans_processed': view_plans_processed,
-        'my_plans_processed': my_plans_processed,
+        'my_plans_processed'  : my_plans_processed,
     }
     return render(request, 'saves.html', context=context)
 
 
 def remove_likeplan(request, plan_id):
-    user = request.user
-    plan = user.like_plans.get(id=plan_id)
-    plan.like_users.remove(user)
+    login_user = request.user
+    plan = login_user.like_plans.get(id=plan_id)
+    plan.like_users.remove(login_user)
     return HttpResponse('Success', status = 200)
 
 
 def add_likeplan(request, plan_id):
-    user = request.user
+    login_user = request.user
     plan = Plan.objects.get(id=plan_id)
-    plan.like_users.add(user)
+    plan.like_users.add(login_user)
     return HttpResponse('Success', status = 200)
 
 def remove_plan(request, plan_id):
-    user = request.user
+    login_user = request.user
     plan = Plan.objects.get(id=plan_id)
     print(f'{plan.title} 삭제')
     plan.delete()
     return HttpResponseRedirect(reverse('Day_Pl:saves'))
 
 def modify_plan(request, plan_id):
-    user = request.user
+    login_user = request.user
     plan = Plan.objects.get(id=plan_id)
     print(f'{plan.title} 수정')
     plan_places = PlanPlace.objects.filter(plan = plan).order_by('order')
@@ -119,21 +142,22 @@ def modify_plan(request, plan_id):
     current_date = date.today()
 
     context = {
-        'id' : plan.id,
-            'uuid' : plan.uuid,
-            'title' : plan.title,
-            'created_at' : plan.created_at,
-            'modified_at' : plan.modified_at,
-            'hashtag_area' : plan.hashtag_area,
-            'hashtag_type' : plan.hashtag_type,
-            'hashtag_pick' : plan.hashtag_pick,
-            'memo' : plan.memo,
-            'total_time' : plan.total_time,
-            'public' : plan.public,
-            'like_count' : plan.like_users.count(),
-            'plan_places' : plan_places,
-            'places' : places,
-            'placetypes' : placetypes,
-            'current_date' : current_date,
+        'create_user'  : plan.user,
+        'id'           : plan.id,
+        'uuid'         : plan.uuid,
+        'title'        : plan.title,
+        'created_at'   : plan.created_at,
+        'modified_at'  : plan.modified_at,
+        'hashtag_area' : plan.hashtag_area,
+        'hashtag_type' : plan.hashtag_type,
+        'hashtag_pick' : plan.hashtag_pick,
+        'memo'         : plan.memo,
+        'total_time'   : plan.total_time,
+        'public'       : plan.public,
+        'like_count'   : plan.like_users.count(),
+        'plan_places'  : plan_places,
+        'places'       : places,
+        'placetypes'   : placetypes,
+        'current_date' : current_date,
     }
     return render(request, 'modify_plan.html', context=context)
